2441d0792f94e8e08c46e0652ca546998119321f
Aldo. D Perez <aldoshin@gmail.com>
2015-04-16 17:52:59 -0400

    scrollspy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#navbar'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   get 'lab1' => 'static_pages#lab1'
   get 'navbar' => 'static_pages#navbar'
   get 'indexnavbar' => 'static_pages#index_navbar'
-  
+  get 'tabs' => 'static_pages#tabs'
+  get 'scrollspy' => 'static_pages#scrollspy'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
